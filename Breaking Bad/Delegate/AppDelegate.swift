656d43cd74797171de5caf278e3dba1811c6928a
Abdallah <abdallah@Abdallahs-MacBook-Pro.local>
2021-06-25 05:20:31 +0200

    refactor networking amd addmake  Generics

--- Breaking Bad/Delegate/AppDelegate.swift
+++ Breaking Bad/Delegate/AppDelegate.swift
@@ -17,28 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         IQKeyboardManager.shared.enable = true
-    //    setupSideMenu()
         
         return true
     }
 
-//    func setupSideMenu() {
-//
-//        let menu = UISideMenuNavigationController(rootViewController:SideMenuVC.create() )
-//        menu.isNavigationBarHidden = true
-//        SideMenuManager.default.menuLeftNavigationController = menu
-//
-//        SideMenuManager.default.menuFadeStatusBar = false
-//        SideMenuManager.default.menuAllowPushOfSameClassTwice = false
-//        SideMenuManager.default.menuPresentMode = .menuSlideIn
-//        SideMenuManager.default.menuPushStyle = .default
-//        SideMenuManager.default.menuWidth = (window?.frame.width)! * 0.7
-//        SideMenuManager.default.menuDismissOnPush = true
-//        SideMenuManager.default.menuAnimationBackgroundColor = .black
-//        SideMenuManager.default.menuAnimationFadeStrength = 0.5
-//
-//
-//    }
+
     
 
 }
